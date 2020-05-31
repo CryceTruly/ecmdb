7497ecf630483017e4edc2bdf15b6205fd298612
crycetruly <crycetruly@gmail.com>
2020-05-31 13:31:40 +0200

    make it better

--- company/admin.py
+++ company/admin.py
@@ -3,4 +3,15 @@ from .models import Company
 # Register your models here.
 
 
-admin.site.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        """Ensure the admin cannot create new Company Instances."""
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        """Ensure the admin cannot delete the existing Company Instance."""
+        return False
+
+
+admin.site.register(Company, CompanyAdmin)
