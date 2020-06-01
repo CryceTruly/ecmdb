e2e2f3395813dffda5465cf2a1be3d5f77c188cb
crycetruly <crycetruly@gmail.com>
2020-06-01 10:07:08 +0200

    ch(files): server files

--- eacmdb/urls.py
+++ eacmdb/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("reports/", include("reports.urls")),
     path('admin/', admin.site.urls),
 
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
