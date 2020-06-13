353a7024f762a13bec736a2cb34dabd22331a26b
crycetruly <crycetruly@gmail.com>
2020-06-13 07:35:11 +0200

    ch(data): Allow Export data to Excel

--- expenses/models.py
+++ expenses/models.py
@@ -16,7 +16,7 @@ class Expense(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     approved_at = models.DateTimeField(null=True, blank=True)
     submitted_by_name = models.CharField(max_length=255)
-    approval_date = models.DateField(auto_now=True)
+    approval_date = models.DateField(null=True)
     status = models.CharField(
         max_length=255, choices=EXPENSE_STATUSES, default='PENDING')
     requester = models.ForeignKey(settings.AUTH_USER_MODEL,
